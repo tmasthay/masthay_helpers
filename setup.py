6777c988a02a529bd8d80526efda3a37397d49e5
tmasthay <tyler@ices.utexas.edu>
2023-04-27 14:18:54 -0700

    modify setup.py to configure properly -- seems to work

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from setuptools.extension import Extension
 from Cython.Build import cythonize
 
 extensions = [
     Extension(
-        "global_helpers_cython",
-        sources=["global_helpers_cython.pyx"],
+        "helpers.global_helpers_cython",
+        sources=["helpers/global_helpers_cython.pyx"],
         extra_compile_args=["-O3"],
         extra_link_args=["-O3"],
     )
 ]
 
 setup(
-    name="Helpers",
+    name="helpers",
     version="0.1",
     author="Tyler Masthay",
     description="Helper functions for repetitive and useful tasks",
-    packages=["helpers"],
+    packages=find_packages(),
     install_requires=["numpy", "matplotlib"],
     ext_modules=cythonize(extensions, compiler_directives={"language_level": "3"}),
     classifiers=[
