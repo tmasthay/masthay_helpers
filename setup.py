b3c2769b8570c5a5f2af6d7a301d4b9751886a54
tmasthay <tyler@ices.utexas.edu>
2023-08-22 15:13:47 -0500

    Change version to indicate cython omissions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="masthay_helpers",
-    version="0.1",
+    version="0.2",
     author="Tyler Masthay",
     description="Helper functions for repetitive and useful tasks",
     python_requires=">=3.10",
