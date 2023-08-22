b446959973f48f53a576eadbe01643981adc4553
tmasthay <tyler@ices.utexas.edu>
2023-08-22 14:31:52 -0500

    Remvoe cython __init__ directives

--- masthay_helpers/__init__.py
+++ masthay_helpers/__init__.py
@@ -1,10 +1,8 @@
 __all__ = [
     "typlotlib",
     "global_helpers",
-    "import_env",
-    "global_helpers_cython"
+    "import_env"
 ]
 from .typlotlib import *
 from .global_helpers import *
 from .import_env import *
-from .global_helpers_cython import *
