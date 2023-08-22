86281b2920b55adacea293f23beaa4172f776449
tmasthay <tyler@ices.utexas.edu>
2023-08-22 16:30:30 -0500

    Actually fix URL syntax

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="masthay_helpers",
-    version="0.2.4",
+    version="0.2.5",
     author="Tyler Masthay",
     description="Helper functions for repetitive and useful tasks",
     long_description=open('README.md').read(),
