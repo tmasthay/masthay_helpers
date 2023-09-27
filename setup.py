1e4ef7ddc4b20a4f46179bf5470e862bb2214ec9
Tyler Masthay <tyler@ices.utexas.edu>
2023-09-27 12:58:47 -0500

    CONFIG: Update version number to 0.2.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="masthay_helpers",
-    version="0.2.8",
+    version="0.2.9",
     author="Tyler Masthay",
     description="Helper functions for repetitive and useful tasks",
     long_description=open('README.md').read(),
