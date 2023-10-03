04459d18a57012afa1fd46e47927140f49771ac4
tmasthay <tyler@ices.utexas.edu>
2023-10-03 13:07:50 -0500

    FEATURE: Add printj syntactic sugar

--- masthay_helpers/global_helpers.py
+++ masthay_helpers/global_helpers.py
@@ -24,53 +24,6 @@ def conda_include_everything():
     os.system(cmd)
 
 
-def inside_quad(p, f):
-    face = np.array(f)
-
-    def inside_triangle(p, A, B, C):
-        AB = B - A
-        BC = C - B
-        AP = p - A
-        BP = p - B
-        magAB = np.linalg.norm(AB) ** 2
-        magBC = np.linalg.norm(BC) ** 2
-        dotAP = np.dot(AP, AB)
-        dotBP = np.dot(BC, BP)
-        testA = 0 <= dotAP and dotAP <= magAB
-        testB = 0 <= dotBP and dotBP <= magBC
-        return testA and testB
-
-    A = face[0]
-    B = face[1]
-    C = face[2]
-    D = face[3]
-    return inside_triangle(p, A, B, C) or inside_triangle(p, A, C, D)
-
-
-def segment_quad_intersect(a, b, face):
-    v = np.array(b - a)
-    p1 = face[1] - face[0]
-    p2 = face[2] - face[0]
-    w = np.cross(p1, p2)
-    beta = np.dot(w, v)
-    input(w)
-    input(v)
-    input(beta)
-    if not np.isclose(beta, 0.0):
-        gamma = np.dot(w, np.array(face[0] - a))
-        candidate = a + gamma / beta * v
-        inside_seg = 0 <= gamma and gamma <= beta
-        input(gamma)
-        input(a)
-        input(v)
-        input(gamma / beta)
-        input('candidate = %s' % candidate)
-        input(inside_seg)
-        return inside_quad(candidate, face) and inside_seg, candidate
-    else:
-        return inside_quad(a, face), a
-
-
 def get_dependencies():  # Run the grep command using subprocess
     grep_command = "grep -rE '^(import|from .* import)' --include='*.py' ."
     result = sco(grep_command)
@@ -161,7 +114,7 @@ def path_up(path, n=1):
     return os.sep.join(path_tokens[:-n])
 
 
-def justify_lines(lines, demarcator='&', align='ljust', extra_space=0):
+def justify_lines(lines, *, demarcator='&', align='ljust', extra_space=0):
     # Step (1)
     l = [line.split(demarcator) for line in lines]
 
@@ -193,6 +146,15 @@ def justify_lines(lines, demarcator='&', align='ljust', extra_space=0):
         formatted_lines.append(' '.join(formatted_line))
 
     return "\n".join(formatted_lines)
+
+
+def printj(lines, *, demarcator='&', align='ljust', extra_space=0, **kw):
+    print(
+        justify_lines(
+            lines, demarcator=demarcator, align=align, extra_space=extra_space
+        ),
+        **kw,
+    )
 
 
 def var_name(tmp, calling_context):
