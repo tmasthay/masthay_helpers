86c876ae5cb3a6c302af645927fa7b12d892e0a1
tmasthay <tyler@ices.utexas.edu>
2024-02-21 00:58:12 -0600

    CONFIG: Update __init__

--- mh/__init__.py
+++ mh/__init__.py
@@ -2,9 +2,11 @@ __all__ = [
     "core",
     "import_env",
     "typlotlib",
+    "core_legacy",
     "jupyter"
 ]
 from .core import *
 from .import_env import *
 from .typlotlib import *
+from .core_legacy import *
 from .jupyter import *
