73a66a77bbb00df1e03ca426d4c9f90d17c6b226
tmasthay <tyler@ices.utexas.edu>
2024-02-21 01:08:57 -0600

    FEATURE: Add typlotlib_legacy

--- mh/__init__.py
+++ mh/__init__.py
@@ -1,11 +1,13 @@
 __all__ = [
     "core",
+    "typlotlib_legacy",
     "import_env",
     "typlotlib",
     "core_legacy",
     "jupyter"
 ]
 from .core import *
+from .typlotlib_legacy import *
 from .import_env import *
 from .typlotlib import *
 from .core_legacy import *
