4f348923062eb1ebb4abe1d7257b499efff54a80
tmasthay <tyler@ices.utexas.edu>
2024-03-16 23:30:44 -0500

    CONFIG: Increment version 0.2.93 -> 0.2.94

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="masthay_helpers",
-    version="0.2.93",
+    version="0.2.94",
     author="Tyler Masthay",
     description="Helper functions for repetitive and useful tasks",
     long_description=open("README.md").read(),
