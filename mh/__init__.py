18a283e9dc6fef4d489bdf1323686c4151b85ad1
tmasthay <tyler@ices.utexas.edu>
2024-03-16 23:30:20 -0500

    CONFIG: .pre-commit-config.yaml and fix __init__

--- mh/__init__.py
+++ mh/__init__.py
@@ -6,13 +6,11 @@ __all__ = [
     "core_legacy",
     "errors",
     "jupyter",
-    "out",
 ]
 from .core import *
 from .core_legacy import *
 from .errors import *
 from .import_env import *
 from .jupyter import *
-from .out import *
 from .typlotlib import *
 from .typlotlib_legacy import *
