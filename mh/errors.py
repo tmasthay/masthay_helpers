12eb8c045bafc973a47f90dc7c94766e22418ed2
tmasthay <tyler@ices.utexas.edu>
2024-08-12 09:44:38 -0500

    FEATURE: ShapeError

--- mh/errors.py
+++ mh/errors.py
@@ -1,2 +1,19 @@
 class NotEnoughResourcesError(Exception):
     pass
+
+
+class ShapeError(Exception):
+    def __init__(self, *, expected, data, field, msg=''):
+        if not hasattr(data, 'shape'):
+            raise ValueError(
+                "Expected data to have a shape attribute, check data type"
+                f" below\n    {type(data)=}"
+            )
+        self.data_shape = data.shape
+        self.expected = expected
+        self.field = field
+        self.msg = msg
+        super().__init__(
+            f"Shape Mismatch!\n    Expected{expected} for {field}, got"
+            f" {data.shape}\n{msg}"
+        )
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="masthay_helpers",
-    version="0.2.104",
+    version="0.2.105",
     author="Tyler Masthay",
     description="Helper functions for repetitive and useful tasks",
     long_description=open("README.md").read(),
