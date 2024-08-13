1fb394f0b5213cf526cdc001c8aa916d44e40a9a
tmasthay <tyler@ices.utexas.edu>
2024-08-13 00:14:04 -0500

    SAFETY: Can maybe ignore these?

--- masthay_helpers/jupyter.py
+++ masthay_helpers/jupyter.py
@@ -82,7 +82,16 @@ def plot_series(*, data, rules, merge, idx, kw):
     for i in range(data.shape[0]):
         idx_lcl = tuple([i] + list(idx))
         r = rules(idx=idx_lcl, **kw)
+<<<<<<< Updated upstream
         curr = r['plot_type'](data[idx_lcl], **r['loop']).opts(**r['opts'])
+=======
+<<<<<<< HEAD
+        curr = r["plot_type"](data[idx_lcl], **r["loop"]).opts(**r["opts"])
+        print(f'opts = {r["opts"]}', flush=True)
+=======
+        curr = r['plot_type'](data[idx_lcl], **r['loop']).opts(**r['opts'])
+>>>>>>> 9466b1a5c7e48b38a670b9c110aa4b6cc5649c81
+>>>>>>> Stashed changes
         runner.append(curr)
     return merge(runner)
 
@@ -187,6 +196,13 @@ def iplot_workhorse(*, data_frame, cols=1, rules):
     ):
         dim = 1 if dim == "1D" else 2
         if special_dim_0 == special_dim_1 and dim == 2:
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+            print("NO CHANGE", flush=True)
+=======
+>>>>>>> 9466b1a5c7e48b38a670b9c110aa4b6cc5649c81
+>>>>>>> Stashed changes
             return reactive_plot.last
         special_dims = [special_dim_0, special_dim_1]
         idx = [
